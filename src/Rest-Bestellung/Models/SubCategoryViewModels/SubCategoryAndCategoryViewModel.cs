a01f9951b030b68b5a8192bd2a64599ad2551f82
Eyad_Kelleh <kelleheyad@gmail.com>
2019-05-12 11:11:05 +0200

    will be a link between the category and subcategory.

---
+++ src/Rest-Bestellung/Models/SubCategoryViewModels/SubCategoryAndCategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rest_Bestellung.Models.SubCategoryViewModels
+{
+    public class SubCategoryAndCategoryViewModel
+    {
+
+    }
+}
