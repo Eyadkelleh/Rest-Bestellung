91932f3d757beea3e95f0f70b4cf5b6b6610aa36
Eyad_Kelleh <kelleheyad@gmail.com>
2019-06-12 06:53:37 +0200

    Deleting not needed using

--- src/Rest-Bestellung/Models/Item.cs
+++ src/Rest-Bestellung/Models/Item.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Rest_Bestellung.Models
 {
@@ -30,8 +26,6 @@ namespace Rest_Bestellung.Models
         public int CategoryId { get; set; }
         [ForeignKey("Category")]
         public virtual Category Category { get; set; }
-
-
         [Display(Name = "SubCategory")]
         public int SubCategoryId { get; set; }
         [ForeignKey("SubCategory")]
