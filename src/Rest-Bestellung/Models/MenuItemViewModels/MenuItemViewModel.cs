38b7ad741ced7718dd3e9b1cb25900fda03a9ca1
Eyad_Kelleh <kelleheyad@gmail.com>
2019-08-09 17:52:15 +0200

    This will be the view Model will be using, when we creating or editing item as well as showing the information, when the user is adding new item or deleting an Item.

---
+++ src/Rest-Bestellung/Models/MenuItemViewModels/MenuItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rest_Bestellung.Models;
+
+namespace Rest_BestellungBestellung.Models.MenuItemViewModels
+{
+    public class MenuItemViewModel
+    {
+        public Item MenuItem { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<SubCategory> SubCategories { get; set; }
+
+    }
+}
